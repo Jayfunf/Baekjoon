7b8909dccf39b499a7fe84cb77cb32ea91afcb5e
Jayfunf <simh3077@gmail.com>
2021-02-02 16:50:25 +0900

    Clear

--- HelloWorld.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    for(int i = 0; i<10; i++){
-        printf("HelloWorld123123123123 \n");
-    }
-    return 0;
-}
--- HelloWorld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    cout << "Hello World123" << endl;
-    return 0;
-}
