414874ab1c86eb696d4c1e4f46fe5ad1bcc950eb
Jayfunf <simh3077@gmail.com>
2021-02-03 16:51:00 +0900

    12865S1

--- No12865.cpp
+++ No12865.cpp
@@ -15,6 +15,7 @@ int dfs(int idx, int remain) {
 	if (ret != -1)return ret;
 	ret = max(dfs(idx + 1, remain - arr[idx][0]) + arr[idx][1], dfs(idx + 1, remain));
 	return ret;
+
 }
 
 int main() {
@@ -22,4 +23,4 @@ int main() {
 	memset(dp, -1, sizeof(dp));
 	F(i, N)for (int j = 0; j < 2; j++)cin >> arr[i][j];
 	cout << dfs(0, K) << endl;
-}4 7
+}
