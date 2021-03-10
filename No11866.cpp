7eb1244b13c4971de6cd55e5ad6fe9be914d0d62
Jayfunf <simh3077@gmail.com>
2021-03-11 00:24:21 +0900

    11866S2

--- No11866.cpp
+++ No11866.cpp
@@ -1,15 +1,37 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
+#include<queue>
+#include<vector>
 
 using namespace std;
 
+
 int main(){
     int a, b;
-    cin >> a>>b;
+    ios_base::sync_with_stdio(false); //¼Óµµ
 
-    new array[a]
+    cin >> a >> b;
 
+    queue<int> Q;
+    vector<int> V;
 
+    for(int i=0; i<a; i++)
+        Q.push(i+1);
 
-    return 0;
-}
+    for(int j = 0; j<a;j++){
+        for(int k = 0; k<b-1;k++){ 
+            int F=Q.front();
+            Q.pop(); 
+            Q.push(F);
+        }
+        V.push_back(Q.front());
+        Q.pop();
+    }
+    cout<<"<";
+
+    for(int i=0; i<a; i++){
+        cout << V[i];
+        if(i != a-1)
+            cout<<", ";
+    }
+     cout<<">";
+}   
